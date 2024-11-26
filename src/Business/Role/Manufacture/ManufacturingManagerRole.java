fac8740782b67420e22484b0887bfc0d1c0bf904
yuta <linbolin1230@gmail.com>
2024-11-26 09:19:06 -0500

    add manufacture roles

---
+++ src/Business/Role/Manufacture/ManufacturingManagerRole.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Role.Manufacture;
+
+import Business.Role.Role;
+
+/**
+ *
+ * @author yuta
+ */
+public class ManufacturingManagerRole extends Role {
+    public ManufacturingManagerRole() {
+        super("Manufacturing Manager");
+    }
+}

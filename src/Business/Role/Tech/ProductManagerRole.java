3ed738600ef5b3ede5527ee6d11f3dab3299bd64
yuta <linbolin1230@gmail.com>
2024-11-26 09:17:13 -0500

    add tech roles

---
+++ src/Business/Role/Tech/ProductManagerRole.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Role.Tech;
+import Business.Role.Role;
+
+/**
+ *
+ * @author yuta
+ */
+public class ProductManagerRole extends Role {
+    public ProductManagerRole() {
+        super("Product Manager");
+    }
+}
---
+++ src/Business/Role/Tech/PurchasingManagerRole.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Role.Tech;
+
+import Business.Role.Role;
+
+/**
+ *
+ * @author yuta
+ */
+public class PurchasingManagerRole extends Role {
+    public PurchasingManagerRole() {
+        super("Purchasing Manager");
+    }
+}

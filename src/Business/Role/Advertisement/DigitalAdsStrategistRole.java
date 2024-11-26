b1dda3f2f8692d67c6ca725745f0ce4889d96b24
yuta <linbolin1230@gmail.com>
2024-11-26 09:23:23 -0500

    add advertisement role

---
+++ src/Business/Role/Advertisement/DigitalAdsStrategistRole.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Role.Advertisement;
+
+import Business.Role.Role;
+
+/**
+ *
+ * @author yuta
+ */
+public class DigitalAdsStrategistRole extends Role {
+    public DigitalAdsStrategistRole() {
+        super("Digital Ads Strategist");
+    }
+}
